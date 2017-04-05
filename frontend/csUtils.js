0d44e1170d228acdb5de006034a1257b8eb8991f
James Donnelly <james.donnelly11@outlook.com>
2017-04-05 19:06:31 +0100

    james donnelly work on frontend task

--- frontend/csUtils.js
+++ frontend/csUtils.js
@@ -2,13 +2,13 @@ var csApi = (function  () {
 
   var api = {},
       baseUrl = 'https://dea.staging.credsimple.com',
-      path = '/v1/clients_providers/'
+      path = '/v1/clients_providers/',
       token = '3ad6aef59ee542ec881c5bc6593ba9c3',
       id = 20;
 
   api.getData = function(callback) {
-    requestUrl = baseUrl + path + id + '?token=' + token;
-    request = new XMLHttpRequest();
+    var requestUrl = baseUrl + path + id + '?token=' + token;
+    var request = new XMLHttpRequest();
     request.open('get', requestUrl, true);
     request.onload = function(e) {
       var response = request.response;
--- frontend/exercise.js
+++ frontend/exercise.js
@@ -1,1 +1,95 @@
-console.log('Good luck! You can do it!');
+document.addEventListener("DOMContentLoaded", function() {
+
+    var app = (function app() {
+
+        var showExpired = document.getElementById('button-expired');
+        console.log(showExpired);
+
+        showExpired.addEventListener('click', function(e) {
+            var records = document.getElementsByClassName("dea-record");
+            for(var i = 0; i < records.length; i++)
+            {
+                var record = records.item(i);
+                var status = record.dataset.status
+               console.log(status);
+                if (status != 'expired') {
+                    record.style.display = 'none';;
+                }
+            }
+        });
+
+        var cs = function(result) {
+
+            result.sort(function(a,b){
+                var aDate = new Date(a.expiration_date)
+                var bDate = new Date(b.expiration_date)
+                return aDate.getTime() - bDate.getTime()
+            });
+
+            outputResults(result, 'all');
+
+        };
+        csApi.getData(cs);
+
+    });
+
+    app();
+});
+
+
+function toggleDetail(npi) {
+    var displayValue = document.getElementById('detail_' + npi).style.display;
+    if(displayValue === 'block'){
+        document.getElementById('detail_' + npi).style.display = 'none';
+    }else{
+        document.getElementById('detail_' + npi).style.display = 'block';
+    }
+}
+
+function outputResults(result, status){
+    var records = '';
+    var now = new Date();
+    var status = '';
+
+    for(var i = 0; i<=30; i++){
+        console.log(result[i]);
+
+        var expiryDate = new Date(result[i].expiration_date);
+        var statusHtml = '';
+        if(expiryDate >= now){
+            status = 'active';
+            statusHtml = '<div class="alert alert-active pull-right" >Active</div>'
+        }else{
+            status = 'expired';
+            statusHtml = '<div class="alert alert-expired pull-right" >Expired</div>'
+        }
+
+        records += '<div class="dea-record" data-status="'+ status + '">' +
+            '<div class="row record-header" onclick="toggleDetail(' + result[i].npi + ')">' +
+            '<div class="col-xs-6">' +
+            '<h3>' + result[i].name + '</h3>' +
+            '</div>' +
+            '<div class="col-xs-6">' + statusHtml + '</div>' +
+            '</div>' +
+            '<div id="detail_' + result[i].npi + '" class="record-detail"> ' +
+            '<div class="row">' +
+            '<div class="col-xs-6">' +
+            '<p>Dea Number: ' + result[i].dea_number + '</p>' +
+            '</div>' +
+            '<div class="col-xs-6">Expiration Date: ' + result[i].expiration_date + '</div>' +
+            '</div>' +
+            '<div class="row">' +
+            '<div class="col-xs-6">' +
+            '<p>NPI: ' + result[i].npi + '</p>' +
+            '</div>' +
+            '<div class="col-xs-6">Provider ID: ' + result[i].provider_id + '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+    }
+
+    document.getElementById('dea-records').innerHTML = records;
+}
+
+
+
